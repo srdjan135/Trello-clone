6517b8da1a0d3e99713371c96c795f40a0bc038e
Srdjan <srdjanmihic3@gmail.com>
2026-01-14 10:01:51 +0100

    Adding sidenav

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MatSidenavContainer } from '@angular/material/sidenav';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatSidenavContent } from '@angular/material/sidenav';
+
+import { SidenavComponent } from '../sidenav/sidenav.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSidenavContainer,
+    MatSidenav,
+    MatSidenavContent,
+    SidenavComponent,
+    RouterOutlet,
+  ],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { MatList, MatListItemTitle } from '@angular/material/list';
+import { MatListItem } from '@angular/material/list';
+import { MatListItemIcon } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [MatList, MatListItem, MatListItemIcon, MatListItemTitle, MatIcon],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss'
+  styleUrl: './sidenav.component.scss',
 })
-export class SidenavComponent {
-
-}
+export class SidenavComponent {}
