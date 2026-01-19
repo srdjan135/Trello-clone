134ff39ef7b014ce0c12b1570737d00868dfe1a0
Srdjan <srdjanmihic3@gmail.com>
2026-01-19 17:07:42 +0100

    Adding dropdown menus to the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatFormField, MatPrefix } from '@angular/material/form-field';
 import { MatLabel } from '@angular/material/form-field';
-import { MatSuffix } from '@angular/material/form-field';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatInput } from '@angular/material/input';
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatTooltip } from '@angular/material/tooltip';
+import { MatMenu } from '@angular/material/menu';
+import { MatMenuItem } from '@angular/material/menu';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-header',
@@ -19,10 +23,17 @@ import { MatToolbar } from '@angular/material/toolbar';
     MatIconButton,
     MatInput,
     MatToolbar,
+    MatTooltip,
+    MatMenu,
+    MatMenuItem,
+    MatMenuTrigger,
+    MatDivider,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   userInitials = 'P';
+  username = 'Pele';
+  email = 'pelepelovic9@gmail.com';
 }
